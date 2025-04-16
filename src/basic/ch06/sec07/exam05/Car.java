1fd8c95a45fa59efccef33e1d75bcca24ade2911
ockko <ocko119@gmail.com>
2025-04-16 17:49:40 +0900

    클래스 기본 푸는 중

---
+++ src/basic/ch06/sec07/exam05/Car.java
@@ -0,0 +1,23 @@
+package basic.ch06.sec07.exam05;
+
+public class Car {
+    // 필드 선언
+    String company = "현대자동차";
+    String model;
+    String color;
+    int maxSpeed;
+
+    Car(String model) {
+        this(model, "빨강", 250);
+    }
+
+    Car(String model, String color) {
+        this(model, color, 250);
+    }
+
+    Car(String model, String color, int maxSpeed) {
+        this.model = model;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+    }
+}
